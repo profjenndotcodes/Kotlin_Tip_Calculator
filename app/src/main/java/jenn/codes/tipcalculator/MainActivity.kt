067cad617863df06ad36a04926f45445157943fb
jenn dot codes <reddunrogue@gmail.com>
2019-01-22 20:01:20 -0700

    Put a NumberFormatter to demonstrate currency conversion

--- app/src/main/java/jenn/codes/tipcalculator/MainActivity.kt
+++ app/src/main/java/jenn/codes/tipcalculator/MainActivity.kt
@@ -3,6 +3,8 @@ package jenn.codes.tipcalculator
 import android.icu.text.NumberFormat
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,7 +14,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val calcTipButton = findViewById<Button>(R.id.btnCalc)
+        val resultText = findViewById<TextView>(R.id.txtResult)
 
+        calcTipButton.setOnClickListener {
+            resultText.setText(currencyFormat.format(7.89645).toString())
+
+            // Hide the keyboard.
+            //val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            //imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
 
 
     }
